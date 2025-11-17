32a269941e2b7aaae5832fd6c0063da07f98037f
stpatriarch <chinaryannarek@gmail.com>
2025-11-17 23:31:31 +0400

    starting point

---
+++ src/conotes/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from importlib.metadata import version, PackageNotFoundError
+
+
+
+__title__ = "conotes"
+__description__ = "Note taking tool from console."
+__url__ = "https://github.com/stpatriarch/conotes"
+__version__ = version("conotes")
+__author__ = "stpatriarch"
+__author_email__ = "chinaryannarek@gmail.com"
+__license__ = "MIT"
